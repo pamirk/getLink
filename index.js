57a1262ba2dccfca3864c281a3b0e77d20d7387d
pamir <pmysteryk@gmail.com>
2020-12-02 12:15:38 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const cron = require('node-cron');
+const express = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+app = express();
+// Schedule tasks to be run on the server.
+let vlink = null
+
+app.get('/gl', (req, res, next) => {
+    return res.send(vlink)
+});
+
+cron.schedule('0 0 */3 * * *', function () {
+    fetch("https://coursehunter.net/course/next-js",)
+        .then(res => res.text())
+        .then(html => {
+            const parsedHTML = cheerio.load(html);
+            const data = JSON.parse(parsedHTML('script').get()[0].children[0].data);
+            vlink = data['@graph'][0].contentUrl.split('/')[4]
+        })
+});
+app.listen(3000);
