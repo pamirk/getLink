e3dfab92b129ee8448521f36cab90213756605cf
pamir <pmysteryk@gmail.com>
2020-12-02 19:48:46 +0500

    cranjob added

---
+++ cranjob.js
@@ -0,0 +1,12 @@
+export let vlink = null
+
+function sayHello() {
+    fetch("https://coursehunter.net/course/next-js",)
+        .then(res => res.text())
+        .then(html => {
+            const parsedHTML = cheerio.load(html);
+            const data = JSON.parse(parsedHTML('script').get()[0].children[0].data);
+            vlink = data['@graph'][0].contentUrl.split('/')[4]
+        })
+}
+sayHello();
